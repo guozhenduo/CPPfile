f4c0f2cbea0fbc81098813f2b46018cfae1da4c0
suchenguo <guozhenduo1@163.com>
2020-07-01 18:28:57 +0800

    No01.cpp

---
+++ No02.cpp
@@ -0,0 +1,16 @@
+#ifdef __cplusplus
+extern "C" {
+	#endif
+	#include <stdio.h>
+	#ifdef __cplusplus
+}
+	#endif 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hello world \n";
+	cout << "你好世界 \n";
+	return 0;
+}
